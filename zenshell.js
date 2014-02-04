710e627710ab921cf298e7b737e9fc6470915a4a
J.D. Manuel <jdmanuel@me.com>
2014-02-04 10:57:52 -0800

    Require full URL

--- zenshell.js
+++ zenshell.js
@@ -204,7 +204,7 @@ function postTicket(count) {
   };
 
   request.post({
-      url: "https://" + host +"/api/v2/tickets.json",
+      url: host + "/api/v2/tickets.json",
       auth: {
         'user': email,
         'pass': password,
